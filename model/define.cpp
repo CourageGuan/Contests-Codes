073216f6d1fff1213154215de3df18bb4bfb52e8
Alex Guan <nobystander96@gmail.com>
2014-12-17 23:43:31 +0800

    新文件:       UVA/README 	重命名:       UVA/UVA220Othello.cpp -> UVA/UVA220Othello(WA).cpp 	新文件:       model/define.cpp

---
+++ model/define.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+#include<cstdio>
+#include<cstring>
+#include<cmath>
+//#include<ctime>
+
+#include<algorithm>
+#include<vector>
+#include<stack>
+#include<queue>
+#include<deque>
+#include<list>
+#include<set>
+#include<map>
+
+using namespace std;
+//ios::sync_with_stdio(false);  //speed
+
+typedef vector<int> VI;
+typedef map<string,int> MSI;
+typedef map<int,int> MII;
+typedef pair<int,int> PII;
+typedef long long LL;
+typedef double DB;
+
+
+#define F(i,a,b) for(int i=a;i<=b;i++)
+#define Fd(i,a,b) for(int i=a;i>=b;i--)
+#define P(a) cout<<a<<endl 
+#define SET(a,t) memset(a,t,sizeof(a))
+#define SETS(ST) while(!ST.empty()) ST.pop();
+#define DEBUG puts("!BUG!")
+#define PM(x,y,a) F(i,0,x){F(j,0,y) cout<<a[i][j]<<" ";cout<<endl;} //Print Matrix
+#define FRER freopen(".in","r",stdin)
+#define FREW freopen(".out","r",stdout)
+
+
+const double PI=acos(-1.0);
+const int MAXN=0x7fffffff;
+const int MINN=0x8fffffff;
+const int EPS=1e-7;
+
+
+/*
+   on ne voit bien qu'avec le coeur.l'essentiel est invisible pour les yeux
+                                                                            */
+
+
+void  solve()
+{
+
+}
+int main()
+{
+  solve();
+}
