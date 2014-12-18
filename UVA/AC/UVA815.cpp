436b3131963bc767baec10b29b8593c3681d40b9
Alex Guan <nobystander96@gmail.com>
2014-12-18 15:54:32 +0800

    修改:         CodeForces/#282(div2)/P1.cpp 	修改:         CodeForces/#282(div2)/P2.cpp 	修改:         UVA/AC/UVA1589Xiangqi.cpp 	修改:         UVA/AC/UVA201Squares.cpp 	修改:         UVA/AC/UVA253.cpp 	新文件:       UVA/AC/UVA815.cpp 	修改:         UVA/README 	删除:         UVA/README~ 	重命名:       UVA/UVA220Othello(WA).cpp -> UVA/UVA220Othello WA.cpp 	重命名:       UVA/UVA815(WA).cpp -> UVA/UVA815 WA.cpp 	修改:         model/define.cpp 	修改:         "model/\346\214\207\345\256\232\345\255\220\344\270\262\346\233\277\346\215\242.cpp"

--- UVA/AC/UVA815.cpp
+++ UVA/AC/UVA815.cpp
@@ -26,33 +26,68 @@ typedef double DB;
 
 #define F(i,a,b) for(int i=a;i<=b;i++)
 #define Fd(i,a,b) for(int i=a;i>=b;i--)
-#define P(a) cout<<a<<endl 
+#define C(a) cout<<a<<endl
 #define SET(a,t) memset(a,t,sizeof(a))
 #define SETS(ST) while(!ST.empty()) ST.pop();
 #define DEBUG puts("!BUG!")
 #define PM(a,x,y) F(i,0,x){F(j,0,y) cout<<a[i][j]<<" ";cout<<endl;} //Print Matrix
 
-#define FRER freopen(".in","r",stdin)
-#define FREW freopen(".out","W",stdout)
+#define FRER freopen("test","r",stdin)
+#define FREW freopen("1","w",stdout)
 
 
 const double PI=acos(-1.0);
 const int MAXN=0x7fffffff;
 const int MINN=0x8fffffff;
-const int EPS=1e-7;
-const int N=
-
+const double EPS=1e-7;
+const int N=35;
 
 /*
    on ne voit bien qu'avec le coeur.l'essentiel est invisible pour les yeux
                                                                             */
 
-
-void  solve()
+void  solve(int m,int n)
 {
-
+  //DEBUG;
+  double avg=0.0,ans=0.0;
+  double reg[N*N];
+  int sum=n*m;
+  SET(reg,0.0);
+  F(i,1,sum)
+    scanf("%lf",&reg[i]);
+  sort(reg+1,reg+sum+1);
+  double v;
+  scanf("%lf",&v);
+  v/=100.0;
+  //C(v);
+  //F(i,1,sum) cout<<reg[i]<<" ";
+  F(i,1,sum)
+    if((avg+(reg[i+1]-reg[i])*i)<=v && i<sum)
+      avg+=(reg[i+1]-reg[i])*i;
+	  else{
+	    //if(abs(v-avg)<EPS) ans=(i-1)*1.0/sum;
+      ans=i*1.0/sum;
+	    //C(ans);
+	    avg=reg[i]+(v-avg)/i;
+	    break;
+    }
+  //PM(reg,m+1,n+1);
+  //DEBUG;
+  //C(avgd);
+  printf("Water level is %.2lf meters.\n",avg);
+  printf("%.2lf percent of the region is under water.\n",ans*100);
 }
+
 int main()
 {
-  solve();
+  //FRER;
+  //FREW;
+  int m,n,_=1;
+  while(scanf("%d%d",&m,&n)==2&&m&&n)
+  {
+    printf("Region %d\n",_++);
+    solve(m,n);
+    printf("\n");
+  }
+  return 0;
 }
