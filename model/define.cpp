df4557bdd9aeab9bdbb214fa89e4d7172db33ec2
Alex Guan <nobystander96@gmail.com>
2014-12-22 17:00:35 +0800

    r define.cpp

--- model/define.cpp
+++ model/define.cpp
@@ -1,62 +1,74 @@
-#include<iostream>
-#include<cstdio>
-#include<cstdlib>
-#include<cstring>
-#include<cmath>
-#include<ctime>
+//#pragma comment(linker, "/STACK:1024000000,1024000000")
+#include <iostream>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
 
-#include<algorithm>
-#include<vector>
-#include<stack>
-#include<queue>
-#include<deque>
-#include<list>
-#include<set>
-#include<map>
-
+#include <algorithm>
+#include <list>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <set>
+#include <map>
+#include <string>
 using namespace std;
-//ios::sync_with_stdio(false);  //speed
 
+typedef long long LL;
 typedef vector<int> VI;
 typedef map<string,int> MSI;
 typedef map<int,int> MII;
 typedef pair<int,int> PII;
-typedef long long LL;
 
 #define F(i,a,b) for(int i=a;i<=b;i++)
 #define Fd(i,a,b) for(int i=a;i>=b;i--)
-#define C(a) cout<<a<<endl
+#define ALL(x) x.begin(),x.end()
 #define SET(a,t) memset(a,t,sizeof(a))
 #define SETS(ST) while(!ST.empty()) ST.pop();
 #define DEBUG puts("OK")
-#define PM(a,x,y) F(i,0,x){F(j,0,y) cout<<a[i][j]<<" ";cout<<endl;} //Print Matrix
+#define C(a) cout<<"~~"<<a<<"~~"<<endl
+#define PM(a,x,y) F(i,0,x){F(j,0,y) cout<<a[i][j]<<" ";cout<<endl;} 
 
 #define FRER freopen("test","r",stdin)
 #define FREW freopen("1","w",stdout)
 
-<template>T
-bool cmp(T a,T b){return a>b;}
+const int inf = 0x7fffffff;
+const int _inf= 0x8fffffff;
+const LL  INF = 1e18;
+const double EPS = 1e-8;
+const double pi = acos(-1.0);
 
-const double pi=acos(-1.0);
-const int inf=0x7fffffff;
-const int _inf=0x8fffffff;
-const LL  INF=1e18;
-const int EPS=1e-7;
+double ROUNDF(double x,int n){
+	double t1,t2-modf(x,&t1);
+	double t3=pow(10,n);
+	long t4=long(t2*t3+0.5);
+	x=t1+t4/t3;
+	return x;
+}
 
-const int N=    ;//The Range of Description
+int gcd(int a,int b){return b==0?a:gcd(b,a%b);}
+
+/*-----  on ne voit bien qu avec le coeur.l essentiel est invisible pour les yeux  -----*/
 
 
-/*
-   on ne voit bien qu'avec le coeur.l'essentiel est invisible pour les yeux
-                                                                            */
+void init()
+{
 
+}
 
-void  solve()
+void solve()
 {
 
 }
 
 int main()
 {
-  return 0;
+	//FRER;
+	//FREW;
+
+	//printf("\nTIME WASTED  %.2f s\n",(double)clock()/CLOCK_PRE_SEC);
+    return 0;
 }
+
